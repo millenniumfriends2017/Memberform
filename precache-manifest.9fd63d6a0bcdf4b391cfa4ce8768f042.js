ddd28154020b1f30a500d962a9f7ec6566738c78
millenniumfriends2017 <66288536+millenniumfriends2017@users.noreply.github.com>
2020-06-02 08:44:21 +0600

    addad main files

---
+++ precache-manifest.9fd63d6a0bcdf4b391cfa4ce8768f042.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ee6694293ba3db302766bc1bc55eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88602f75dd1067228527",
+    "url": "/static/css/2.5c0f8a16.chunk.css"
+  },
+  {
+    "revision": "a99d611343e39b0743b4",
+    "url": "/static/css/main.39841786.chunk.css"
+  },
+  {
+    "revision": "88602f75dd1067228527",
+    "url": "/static/js/2.a9b0b2eb.chunk.js"
+  },
+  {
+    "revision": "3df69a78d809b9629cd2f69d31cecaad",
+    "url": "/static/js/2.a9b0b2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99d611343e39b0743b4",
+    "url": "/static/js/main.93f4b3ea.chunk.js"
+  },
+  {
+    "revision": "0203f16c03e0730a00ba",
+    "url": "/static/js/runtime-main.729a5ed5.js"
+  },
+  {
+    "revision": "dfb06311060cf85b1563cee34ba4c1cd",
+    "url": "/static/media/Millennium.dfb06311.png"
+  },
+  {
+    "revision": "4dcb7cb38ef92e6ab13970a52f3d86af",
+    "url": "/static/media/bg.4dcb7cb3.jpg"
+  },
+  {
+    "revision": "839c5e0a2e0183d2c352e0e975e1704c",
+    "url": "/static/media/logo.839c5e0a.jpg"
+  },
+  {
+    "revision": "ea82fbe46c22a2af63f8f4ba2b9d83b2",
+    "url": "/static/media/sign.ea82fbe4.png"
+  }
+]);
